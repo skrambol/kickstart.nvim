c186074ac5ed12578ccaa52b84af74b222d58110
skrambol <systemplado@gmail.com>
2023-09-28 17:21:22 +0800

    customized telescope

--- init.lua
+++ init.lua
@@ -328,12 +328,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
+    -- mappings = {
+    --   i = {
+    --     ['<C-u>'] = false,
+    --     ['<C-d>'] = false,
+    --   },
+    -- },
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        preview_width = 0.4,
+        preview_cutoff = 100,
+      }
     },
+    path_display = { "smart" }
   },
 }
 
@@ -353,7 +361,11 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<F1>', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<F1>', function()
+  require('telescope.builtin').help_tags({
+    previewer = false
+  })
+end, { desc = 'Search help' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
