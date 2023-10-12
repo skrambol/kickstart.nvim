fa6c66f4ef1e5c9030a6ec8f0d9236bf69b39f28
skrambol <systemplado@gmail.com>
2023-10-13 07:33:03 +0800

    indent-blankline update

--- init.lua
+++ init.lua
@@ -68,10 +68,11 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+      indent = { char = '┊' },
+      scope = { show_start = false }
+    }
   },
 
   -- "gc" to comment visual regions/lines
